be7af04c39886f9c14a20e6f93d7c89cbb4a7848
Zachary Burns <114180449+pikkauhn@users.noreply.github.com>
2025-05-07 13:16:07 -0500

    Project Created

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    internal class Note
+    {
+        public string Filename { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+
+        public Note()
+        {
+            Filename = $"{Path.GetRandomFileName()}.notes.txt";
+            Date = DateTime.Now;
+            Text = "";
+        }
+
+        public void Save() =>
+            File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+        public void Delete() =>
+            File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+        public static Note Load(string filename)
+        {
+            filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+            if (!File.Exists(filename))
+            {
+                throw new FileNotFoundException("Unable to find file on local storage.", filename);
+            }
+
+            return
+                new()
+                {
+                    Filename = Path.GetFileName(filename),
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetLastWriteTime(filename)
+                };
+        }
+
+        public static IEnumerable<Note> LoadAll()
+        {
+            string appDataPath = FileSystem.AppDataDirectory;
+            return Directory
+                .EnumerateFiles(appDataPath, "*.notes.txt")
+                .Select(filename => Note.Load(Path.GetFileName(filename)))
+                .OrderByDescending(note => note.Date);
+        }
+    }
+}
